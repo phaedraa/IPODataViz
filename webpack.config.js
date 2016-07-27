bc5d8a32b8ad07ac7f2c02514b91179112af99a1
phaedraa <phaedra.a.r@gmail.com>
2016-07-26 22:16:47 -0700

    Add Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: //"./js",
-    [".js/app.js", "./js/dataHelper.js", "./js/decadeHelper.js", 
-      "./js/dualAxisTimeSeriesHelper.js",
-      "./js/historicalHelper.js", "./js/index.js", "./js/oadash.min.js", 
-      "./js/regressionHelper.js", "./js/yoyHelper.js"
-    ],
+  entry: "./entry.js",
   target: 'node',
   externals: [nodeExternals({
     // this WILL include `jquery` and `webpack/hot/dev-server` in the bundle, as well as `lodash/*` 
     whitelist: ['jquery', 'webpack/hot/dev-server', /^lodash/]
   })],
   output: {
+    path: __dirname,
     filename: "bundle.js"
   },
-  module: { loaders: [{ exclude: /node_modules/ }] },
-}
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  }
+};
