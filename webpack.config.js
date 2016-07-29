104a0cba3ec6d24fb70aa15809256a148f24546b
phaedraa <phaedra.a.r@gmail.com>
2016-07-28 17:56:25 -0700

    Fix Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
-var nodeExternals = require('webpack-node-externals');
-
 module.exports = {
   entry: "./entry.js",
-  target: 'node',
-  externals: [nodeExternals({
-    // this WILL include `jquery` and `webpack/hot/dev-server` in the bundle, as well as `lodash/*` 
-    whitelist: ['jquery', 'webpack/hot/dev-server', /^lodash/]
-  })],
   output: {
     path: __dirname,
     filename: "bundle.js"
